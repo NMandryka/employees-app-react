384af238eac3925c3885fb941038ab45f81ac75b
Nikita <mandrykanikita04@gmail.com>
2022-11-13 19:32:56 +0200

    add form + employees list logic

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,20 +8,60 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-function App() {
-  return (
-    <div className="app">
-        <AppInfo />
+class App extends Component {
 
-        <div className="search-panel">
-            <SearchPanel/>
-            <AppFilter/>
-        </div>
-        
-        <EmployeesList/>
-        <EmployeesAddForm/>
-    </div>
-  );
+	constructor(props) {
+		super(props)
+		this.state = {
+			data: [
+				{name: "John Smith", salary: 2300, increase: false, id: 1},
+				{name: "Mykyta Mandryka", salary: 1250, increase: true, id: 2},
+				{name: "Alex Fsdfa", salary: 5000, increase: false, id: 3} 
+			]
+		}
+	}
+	
+	deleteItem = (id) => {
+		this.setState(({data}) => {
+			return {
+				data: data.filter(item => item.id !== id)
+			}
+		})
+		
+	}
+
+	addItem = (name, salary) => {
+        const newItem = {
+            name, 
+            salary,
+            increase: false,
+            id: this.state.data.length + 1
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr
+            }
+        });
+    }
+
+	render() {
+		return (
+			<div className="app">
+				<AppInfo />
+	
+				<div className="search-panel">
+					<SearchPanel/>
+					<AppFilter/>
+				</div>
+				
+				<EmployeesList 
+				data={this.state.data}
+				onDelete={this.deleteItem}/>
+				<EmployeesAddForm onAdd={this.addItem}/>
+			</div>
+		);
+	}
 }
 
 export default App;
